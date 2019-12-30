6d77ba9d302af35ad45d763b0df053d2c557a691
Mahoney <KPM0626@daugherty.com>
2019-12-30 10:54:21 -0600

    initial commit for project

---
+++ demo/src/test/java/com/gclouddemos/springkmo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gclouddemos.springkmo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
